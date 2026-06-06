95f0ff44da5b89bfd20cabad978c795041892364
viii-fn <irhayeelvisebah@gmail.com>
2026-06-06 06:50:48 +0100

    Potential App route fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,14 +45,14 @@ export const Navbar: React.FC = () => {
           <polyline points="9 22 9 12 15 12 15 22" />
         </svg>
       </NavLink>
-      <NavLink to="/media" style={getLinkStyle}>
+      <NavLink to="/dev" style={getLinkStyle}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="34" height="34" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <rect width="18" height="12" x="3" y="4" rx="2" ry="2" />
           <line x1="2" y1="20" x2="22" y2="20" />
           <line x1="5" y1="16" x2="19" y2="16" />
         </svg>
       </NavLink>
-      <NavLink to="/dev" style={getLinkStyle}>
+      <NavLink to="/media" style={getLinkStyle}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="34" height="34" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
           <path d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z" />
           <circle cx="12" cy="13" r="3" />
