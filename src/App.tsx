fd0809b62ba55e355735928a7fb9d259ca393cdb
viii-fn <irhayeelvisebah@gmail.com>
2026-06-10 05:38:37 +0100

    Done :)

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Home } from './pages/Home';
 import { Dev } from './pages/Dev';
-import { Media } from './pages/Media';
-import { Contact } from './pages/Contact';
 
 export default function App() {
   return (
@@ -17,8 +15,6 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dev" element={<Dev />} />
-          <Route path="/media" element={<Media />} />
-          <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
     </BrowserRouter>
