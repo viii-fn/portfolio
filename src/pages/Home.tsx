1cc618665a8b8340c58412b9649aeb157b56b204
viii-fn <irhayeelvisebah@gmail.com>
2026-08-15 18:10:56 +0100

    Major resume updates - Minor UI updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,9 +17,8 @@ const HeroSection: React.FC = () => {
           <h1 className='text-4xl text-center text-white tracking-widest uppercase font-thin font-sans md:text-7xl'>{user.firstName} {user.lastName}</h1>
           <div className='grid grid-cols-2 gap-3 mt-5'>
             {user.roles.map((role, index) => (
-              <div key={index} className='p-2.5 rounded-4xl text-[#9b9b9b] bg-[#2c2c2c] hover:text-white text-center  transition duration-300 border'>
-                <h1 className='text-4xl animate-pulse text-white mb-3'>{role.years}+ years</h1>
-                <h2 className='md:text-xl'>of experience in {role.career}</h2>
+              <div key={index} className='p-2.5 rounded-4xl bg-[#2c2c2c] hover:text-white text-center  transition duration-300 border'>
+                <h2 className='md:text-xl capitalize animate-pulse'>{role.title}</h2>
               </div>
             ))}
           </div>
@@ -35,11 +34,13 @@ const AboutSection: React.FC = () => {
   return (
     <>
     {elvis.map(user => (
-      <div className='p-5 my-10 mx-auto bg-[#16161685] text-[#9b9b9b] w-[90%] rounded-4xl md:grid md:grid-cols-2 text-lg'>
-        <div className='p-2.5 rounded-2xl my-5'>
-          <h1 className='text-5xl text-white pb-2.5'>Hey there</h1> 
+      <div className='p-5 my-10 mx-auto bg-[#16161685] text-[#9b9b9b] w-[90%] rounded-4xl text-lg'>
+
+          <h1 className='p-2.5 text-5xl text-white pb-2.5'>Hey there, I'm <b className='capitalize'>{user.firstName}</b></h1>
+      <div className='md:grid md:grid-cols-2'>
+        <div className='p-2.5 rounded-2xl my-5'> 
           <h2 className='font-thin'>
-          I'm <b className='capitalize'>{user.firstName}</b>, a self-taught {user.roles.map((role, index) => (<strong key={index}>{role.title}, </strong>))} who is driven by deep passion and obssession for technology, terminals and smooth animations.
+          I'm a self-taught <strong>{user.roles[0].title}</strong> and <strong>{user.roles[1].title}</strong> who is driven by deep passion and obssession for technology, terminals and smooth animations.
           </h2>
         </div>
         <div className='p-2.5 rounded-2xl my-5'>
@@ -49,14 +50,15 @@ const AboutSection: React.FC = () => {
         </div>
         <div className='p-2.5 rounded-2xl my-5'>
           <h2 className='font-thin'>
-            With <strong>over half a decade</strong> studying and understanding <strong>camera angles and color grading</strong>, I've developed exceptional skills with softwares like {user.roles[1].skills.map((skill, index) => (<strong key={index} className='capitalize'>{skill}, </strong>))} and many more to create captivating and realistic scenes from any environment at all.
+            With <strong>over half a decade</strong> studying and understanding <strong>camera angles, creative sound design and color grading</strong>, I've developed exceptional skills with softwares like {user.roles[1].skills.map((skill, index) => (<strong key={index} className='capitalize'>{skill}, </strong>))} and many more to create captivating and realistic scenes from any environment at all.
           </h2>
         </div>
         <div className='p-2.5 rounded-2xl my-5'>
           <h2 className='font-thin'>
-            Design isn’t an afterthought—it’s baked into my workflow from the first wireframe to the final pixel, so every <strong>animation, hover state, and transition</strong> serves a purpose and reinforces the brand story.
+            Design isn’t an afterthought... it’s baked into my workflow from the first wireframe to the final pixel, so every <strong>animation, hover state, and transition</strong> serves a purpose and reinforces the brand story.
           </h2>
         </div>
+      </div>
       </div>
     ))}
     </>
